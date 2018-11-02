f0daa4174134080753f856549d406668bc3c1c0c
Jeff Irion <JeffLIrion@users.noreply.github.com>
2018-11-01 18:47:20 -0700

    Require 'adb-homeassistant', not 'adb>=1.3.0'

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='androidtv',
-    version='0.0.1',
+    version='0.0.2',
     description='Communicate with an Android TV device via ADB over a network.',
     url='https://github.com/JeffLIrion/python-androidtv/',
     license='MIT',
     author='Jeff Irion',
     author_email='jefflirion@users.noreply.github.com',
     packages=['androidtv'],
-    install_requires=['pycryptodome', 'rsa', 'adb>=1.3.0'],
+    install_requires=['pycryptodome', 'rsa', 'adb-homeassistant'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
