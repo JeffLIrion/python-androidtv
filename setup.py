09fcaf2fe04528e42f7ebeb82f80ad09a5830aca
Jeff Irion <jefflirion@users.noreply.github.com>
2019-07-01 06:00:26 -0700

    Bump version to 0.0.17

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='androidtv',
-    version='0.0.16',
+    version='0.0.17',
     description='Communicate with an Android TV or Fire TV device via ADB over a network.',
     url='https://github.com/JeffLIrion/python-androidtv/',
     license='MIT',
     author='Jeff Irion',
     author_email='jefflirion@users.noreply.github.com',
     packages=['androidtv'],
-    install_requires=['rsa', 'adb-homeassistant', 'pure-python-adb-homeassistant'],
+    install_requires=['rsa', 'adb-homeassistant>=1.3.2', 'pure-python-adb-homeassistant>=0.1.7.dev0'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
