940b2199fc3976f165d7f282b00eb12827a9c4ce
Jeff Irion <jefflirion@users.noreply.github.com>
2019-08-30 09:53:54 -0700

    Clean up patchers.py

--- tests/patchers.py
+++ tests/patchers.py
@@ -43,7 +43,7 @@ class ClientFakeSuccess(object):
         return self._devices
 
     def device(self, serial):
-        """Mock the `ClientFake.device` method when it is successful."""
+        """Mock the `Client.device` method when the device is connected via ADB."""
         device = DeviceFake(serial)
         self._devices.append(device)
         return device
@@ -60,7 +60,7 @@ class ClientFakeFail(object):
         return self._devices
 
     def device(self, serial):
-        """Mock the `ClientFake.device` method when it fails."""
+        """Mock the `Client.device` method when the device is not connected via ADB."""
         self._devices = []
         return None
 
@@ -124,104 +124,3 @@ def patch_shell(response=None, cmd_assert=False, error=False):
     if not error:
         return patch('{}.AdbCommandsFakeSuccess.Shell'.format(__name__), shell_success), patch('{}.DeviceFake.shell'.format(__name__), shell_success)
     return patch('{}.AdbCommandsFakeFail.Shell'.format(__name__), shell_fail_python), patch('{}.DeviceFake.shell'.format(__name__), shell_fail_server)
-
-
-
-
-
-
-
-
-
-
-def adb_command_success(self, cmd):
-    """Return ``cmd``."""
-    return cmd
-
-
-def connect_device_success(self, *args, **kwargs):
-    """Return `self`, which will result in the ADB connection being interpreted as available."""
-    return self
-
-
-def connect_device_fail(self, *args, **kwargs):
-    """Raise a socket error."""
-    raise socket_error
-
-
-def adb_shell_python_adb_error(self, cmd):
-    """Raise an error that is among those caught for the Python ADB implementation."""
-    raise AttributeError
-
-
-def adb_shell_adb_server_error(self, cmd):
-    """Raise an error that is among those caught for the ADB server implementation."""
-    raise ConnectionResetError
-
-
-class AdbAvailable(object):
-    """A class that indicates the ADB connection is available."""
-
-    def shell(self, cmd):
-        """Send an ADB shell command (ADB server implementation)."""
-        return cmd
-
-
-class AdbUnavailable(object):
-    """A class with ADB shell methods that raise errors."""
-
-    def __bool__(self):
-        """Return `False` to indicate that the ADB connection is unavailable."""
-        return False
-
-    def shell(self, cmd):
-        """Raise an error that pertains to the Python ADB implementation."""
-        raise ConnectionResetError
-
-
-def patch_shelll(response):
-    def shell(self, cmd):
-        self.shell_cmd = cmd
-        return response
-
-    return patch('androidtv.adb_helper.ADBPython.shell', shell)
-
-
-PATCH_PYTHON_ADB_CONNECT_SUCCESS = patch(
-    "adb.adb_commands.AdbCommands.ConnectDevice", connect_device_success
-)
-PATCH_PYTHON_ADB_COMMAND_SUCCESS = patch(
-    "adb.adb_commands.AdbCommands.Shell", adb_command_success
-)
-PATCH_PYTHON_ADB_CONNECT_FAIL = patch(
-    "adb.adb_commands.AdbCommands.ConnectDevice", connect_device_fail
-)
-PATCH_PYTHON_ADB_COMMAND_FAIL = patch(
-    "adb.adb_commands.AdbCommands.Shell", adb_shell_python_adb_error
-)
-PATCH_PYTHON_ADB_COMMAND_NONE = patch(
-    "adb.adb_commands.AdbCommands.Shell", return_value=None
-)
-
-PATCH_ADB_SERVER_CONNECT_SUCCESS = patch(
-    "adb_messenger.client.Client.device", return_value=AdbAvailable()
-)
-PATCH_ADB_SERVER_AVAILABLE = patch(
-    "androidtv.basetv.BaseTV.available", return_value=True
-)
-PATCH_ADB_SERVER_AVAILABLE2 = patch(
-    "androidtv.adb_helper.ADBServer.available", return_value=True
-)
-PATCH_ADB_SERVER_UNAVAILABLE = patch(
-    "androidtv.adb_helper.ADBServer.available", return_value=False
-)
-PATCH_ADB_SERVER_CONNECT_FAIL = patch(
-    "adb_messenger.client.Client.device", return_value=False
-)
-PATCH_ADB_SERVER_COMMAND_FAIL = patch(
-    "{}.AdbAvailable.shell".format(__name__), adb_shell_adb_server_error
-)
-PATCH_ADB_SERVER_COMMAND_NONE = patch(
-    "{}.AdbAvailable.shell".format(__name__), return_value=None
-)
-
