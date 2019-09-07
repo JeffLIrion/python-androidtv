f7d1a9e55b05ab4241e454af34df07ba3835658e
Jeff Irion <jefflirion@users.noreply.github.com>
2019-09-07 08:06:38 -0700

    Update scripts

--- scripts/git_tag.sh
+++ scripts/git_tag.sh
@@ -3,13 +3,12 @@
 # get the directory of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# get the package name
+PACKAGE=$($DIR/get_package_name.sh)
+
 # get the current version
-VERSION_LINE=$(grep '__version__' $DIR/../androidtv/__init__.py)
-VERSION_TEMP=${VERSION_LINE%"'"}
+VERSION=$($DIR/get_version.sh)
 
-LSTRIP="*'"
-
-VERSION=${VERSION_TEMP##$LSTRIP}
 
 # Announce the tag
 echo "Creating tag v$VERSION"
