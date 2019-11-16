df752207a6f9980c3fdddee0133592743d9ac598
Jeff Irion <JeffLIrion@users.noreply.github.com>
2019-11-16 07:37:38 -0800

    Bump version to 0.0.34 (#123)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='androidtv',
-    version='0.0.33',
+    version='0.0.34',
     description='Communicate with an Android TV or Fire TV device via ADB over a network.',
     url='https://github.com/JeffLIrion/python-androidtv/',
     license='MIT',
