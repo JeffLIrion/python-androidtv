2fd38e775c4db3ca0b23a5b1f6233850bc587ef1
Jeff Irion <jefflirion@users.noreply.github.com>
2019-11-16 07:32:01 -0800

    Bump version to 0.0.34

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,9 +26,9 @@ copyright = '2019, Jeff Irion'
 author = 'Jeff Irion'
 
 # The short X.Y version
-version = '0.0.33'
+version = '0.0.34'
 # The full version, including alpha/beta/rc tags
-release = '0.0.33'
+release = '0.0.34'
 
 
 # -- General configuration ---------------------------------------------------
