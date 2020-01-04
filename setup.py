ae10680b66ab3d078e0184bd4d1fa2a0e6d383fb
Jeff Irion <JeffLIrion@users.noreply.github.com>
2020-01-04 12:17:35 -0800

    Add package info to setup.py (#145)

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open('README.rst') as f:
+    readme = f.read()
+
 setup(
     name='androidtv',
     version='0.0.37',
     description='Communicate with an Android TV or Fire TV device via ADB over a network.',
+    long_description=readme,
+    keywords=['adb', 'android', 'androidtv', 'firetv'],
     url='https://github.com/JeffLIrion/python-androidtv/',
     license='MIT',
     author='Jeff Irion',
@@ -14,6 +19,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
     ],
     test_suite='tests'
 )
