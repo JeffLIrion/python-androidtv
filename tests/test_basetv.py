bf6ee43e3db5e0032a77f5468f446c9446731034
Jeff Irion <jefflirion@users.noreply.github.com>
2020-06-13 09:59:06 -0700

    100% coverage

---
+++ tests/test_basetv.py
@@ -0,0 +1,24 @@
+import sys
+import unittest
+
+
+sys.path.insert(0, '..')
+
+from androidtv import constants
+from androidtv.androidtv.base_androidtv import BaseAndroidTV
+from androidtv.firetv.base_firetv import BaseFireTV
+
+
+class TestBaseTV(unittest.TestCase):
+
+    def test_base_android_tv(self):
+        """Test that ``BaseAndroidTV.__init__`` runs without error.
+
+        """
+        BaseAndroidTV('host')
+
+    def test_base_fire_tv(self):
+        """Test that ``BaseFireTV.__init__`` runs without error.
+
+        """
+        BaseFireTV('host')
