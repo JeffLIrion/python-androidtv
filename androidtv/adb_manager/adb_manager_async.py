ea9864d8d3be95f99412bbdd9d62af1bce4d8ad4
Jeff Irion <JeffLIrion@users.noreply.github.com>
2020-06-13 21:29:14 -0700

    Update adb_manager_async.py

--- androidtv/adb_manager/adb_manager_async.py
+++ androidtv/adb_manager/adb_manager_async.py
@@ -1,6 +1,7 @@
 """Classes to manage ADB connections.
 
-* :py:class:`ADBPython` utilizes a Python implementation of the ADB protocol.
+* :py:class:`ADBPythonAsync` utilizes a Python implementation of the ADB protocol.
+* :py:class:`ADBServerAsync` utilizes an ADB server to communicate with the device.
 
 """
 
