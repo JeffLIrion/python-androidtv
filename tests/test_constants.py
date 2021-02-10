f7fbbea1d9053b31268309f1bda5607ca0bd00ad
Martijn Dijkhuizen <git@martomo.nl>
2021-02-10 15:52:01 +0100

    Rename test helper to properly reflect command being called.

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -16,7 +16,7 @@ class TestConstants(unittest.TestCase):
         return output
 
     @staticmethod
-    def _extract_current_app(dumpsys_output):
+    def _parse_current_app(dumpsys_output):
         return TestConstants._exec('CURRENT_APP="' + dumpsys_output + '" && ' + constants.CMD_PARSE_CURRENT_APP + ' && echo $CURRENT_APP')
 
     @staticmethod
@@ -93,7 +93,7 @@ class TestConstants(unittest.TestCase):
             mCurrentFocus=Window{e74bb23 u0 com.google.android.tvlauncher/com.google.android.tvlauncher.MainActivity}
             mFocusedApp=AppWindowToken{c791eb8 token=Token{a6bad1b ActivityRecord{35dbb2a u0 com.google.android.tvlauncher/.MainActivity t2719}}}
         """
-        current_app = self._extract_current_app(dumpsys_output)
+        current_app = self._parse_current_app(dumpsys_output)
 
         self.assertEqual(current_app, constants.APP_ATV_LAUNCHER)
 
@@ -101,7 +101,7 @@ class TestConstants(unittest.TestCase):
         dumpsys_output = """
             mResumedActivity: ActivityRecord{35dbb2a u0 com.google.android.tvlauncher/.MainActivity t2719}
         """
-        current_app = self._extract_current_app(dumpsys_output)
+        current_app = self._parse_current_app(dumpsys_output)
 
         self.assertEqual(current_app, constants.APP_ATV_LAUNCHER)
 
@@ -110,7 +110,7 @@ class TestConstants(unittest.TestCase):
             mCurrentFocus=Window{c52eaa8 u0 com.sony.dtv.sonysystemservice}
             mFocusedApp=AppWindowToken{11da138 token=Token{19ea99b ActivityRecord{c4d9aa u0 tunein.player/tunein.ui.leanback.ui.activities.TvHomeActivity t2424}}}
         """
-        current_app = self._extract_current_app(dumpsys_output)
+        current_app = self._parse_current_app(dumpsys_output)
 
         self.assertEqual(current_app, constants.APP_TUNEIN)
 
@@ -118,13 +118,13 @@ class TestConstants(unittest.TestCase):
         dumpsys_output = """
             mCurrentFocus=Window{c52eaa8 u0 com.sony.dtv.sonysystemservice}
         """
-        current_app = self._extract_current_app(dumpsys_output)
+        current_app = self._parse_current_app(dumpsys_output)
 
         self.assertEqual(current_app, 'com.sony.dtv.sonysystemservice')
 
     def test_current_app_extraction_with_identifier_as_output(self):
         dumpsys_output = constants.APP_NETFLIX
-        current_app = self._extract_current_app(dumpsys_output)
+        current_app = self._parse_current_app(dumpsys_output)
 
         self.assertEqual(current_app, constants.APP_NETFLIX)
 
