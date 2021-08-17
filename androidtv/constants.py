269a92d9aedca69cf723a347c3b2dc026bb179c8
Agneev Mukherjee <19761269+agneevX@users.noreply.github.com>
2021-08-17 19:23:20 +0530

    Add definition for MrMC Lite (#260)
    * Add definition for MrMC Lite
    
    * Update constants.py

--- androidtv/constants.py
+++ androidtv/constants.py
@@ -344,6 +344,7 @@ APP_KODI = 'org.xbmc.kodi'
 APP_LIVE_CHANNELS = 'com.google.android.tv'
 APP_MIJN_RADIO = 'org.samsonsen.nederlandse.radio.holland.nl'
 APP_MOLOTOV = 'tv.molotov.app'
+APP_MRMC_LITE = 'tv.mrmc.mrmc.lite'
 APP_MX_PLAYER = 'com.mxtech.videoplayer.ad'
 APP_NETFLIX = 'com.netflix.ninja'
 APP_NLZIET = 'nl.nlziet'
@@ -435,6 +436,7 @@ APPS = {APP_AE_TV: 'A&E',
         APP_LIVE_CHANNELS: 'Live Channels',
         APP_MIJN_RADIO: 'Mijn Radio',
         APP_MOLOTOV: 'Molotov',
+        APP_MRMC_LITE: 'MrMC Lite',
         APP_MX_PLAYER: 'MX Player',
         APP_NETFLIX: 'Netflix',
         APP_NLZIET: 'NLZIET',
