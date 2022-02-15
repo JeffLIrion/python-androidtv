813cfbf17847ef867eefdcd14f88bb11f0f6bf6f
Jeff Irion <JeffLIrion@users.noreply.github.com>
2022-02-14 19:26:15 -0800

    Bump the version to 0.0.64 (#305)

--- androidtv/__init__.py
+++ androidtv/__init__.py
@@ -10,7 +10,7 @@ from .constants import DEFAULT_AUTH_TIMEOUT_S, DEFAULT_TRANSPORT_TIMEOUT_S
 from .firetv.firetv_sync import FireTVSync
 
 
-__version__ = "0.0.63"
+__version__ = "0.0.64"
 
 
 def setup(
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,9 +26,9 @@ copyright = '2020, Jeff Irion'
 author = 'Jeff Irion'
 
 # The short X.Y version
-version = '0.0.63'
+version = '0.0.64'
 # The full version, including alpha/beta/rc tags
-release = '0.0.63'
+release = '0.0.64'
 
 
 # -- General configuration ---------------------------------------------------
