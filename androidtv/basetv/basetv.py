8f0cf7d9259160dcad50c4aec946d5208854eaeb
Stephen Goodall <stephen.goodall88@googlemail.com>
2022-02-15 19:26:41 +1100

    Add Option for Custom Audio State command

--- androidtv/basetv/basetv.py
+++ androidtv/basetv/basetv.py
@@ -237,6 +237,18 @@ class BaseTV(object):  # pylint: disable=too-few-public-methods
             return constants.CMD_TURN_ON_FIRETV
 
         return constants.CMD_TURN_ON_ANDROIDTV
+    
+    def _cmd_audio_state(self):
+        """Get the command used to retrieve the current audio state for this device.
+        Returns
+        -------
+        str
+            The device-specific ADB shell command used to determine the current audio state
+        """
+        if constants.CUSTOM_AUDIO_STATE in self._custom_commands:
+            return self._custom_commands[constants.CUSTOM_AUDIO_STATE]
+
+        return constants.CMD_AUDIO_STATE_LEGACY
 
     # ======================================================================= #
     #                                                                         #
