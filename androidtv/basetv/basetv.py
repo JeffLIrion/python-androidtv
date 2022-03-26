4bd5421e107949784b292a5f4a0397875a18e908
Jeff Irion <JeffLIrion@users.noreply.github.com>
2022-03-26 09:41:31 -0700

    Apply suggestions from code review

--- androidtv/basetv/basetv.py
+++ androidtv/basetv/basetv.py
@@ -152,6 +152,7 @@ class BaseTV(object):  # pylint: disable=too-few-public-methods
             and "Chromecast" in self.device_properties.get("model", "")
         ):
             return constants.CMD_CURRENT_APP_GOOGLE_TV
+
         # Is this an Android 11 device?
         if self.DEVICE_ENUM == constants.DeviceEnum.ANDROIDTV and self.device_properties.get("sw_version", "") == "11":
             return constants.CMD_CURRENT_APP11
@@ -177,6 +178,7 @@ class BaseTV(object):  # pylint: disable=too-few-public-methods
             and "Chromecast" in self.device_properties.get("model", "")
         ):
             return constants.CMD_CURRENT_APP_MEDIA_SESSION_STATE_GOOGLE_TV
+
         # Is this an Android 11 device?
         if self.DEVICE_ENUM == constants.DeviceEnum.ANDROIDTV and self.device_properties.get("sw_version", "") == "11":
             return constants.CMD_CURRENT_APP_MEDIA_SESSION_STATE11
