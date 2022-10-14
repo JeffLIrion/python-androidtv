a314499946d12b9984419d04c54fee33afec8a4c
Jo De Boeck <deboeck.jo@gmail.com>
2022-10-14 13:21:28 +0200

    Bump the version to 0.0.69
    Signed-off-by: Jo De Boeck <deboeck.jo@gmail.com>

--- androidtv/__init__.py
+++ androidtv/__init__.py
@@ -10,7 +10,7 @@ from .constants import DEFAULT_AUTH_TIMEOUT_S, DEFAULT_TRANSPORT_TIMEOUT_S
 from .firetv.firetv_sync import FireTVSync
 
 
-__version__ = "0.0.68"
+__version__ = "0.0.69"
 
 
 def setup(
