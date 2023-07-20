e1eaeb260aa3ad39ca969e5495bad095f1d9e90a
Jeff Irion <jefflirion@users.noreply.github.com>
2023-07-19 20:55:38 -0700

    Bump the version to 0.0.71

--- androidtv/__init__.py
+++ androidtv/__init__.py
@@ -10,7 +10,7 @@ from .constants import DEFAULT_AUTH_TIMEOUT_S, DEFAULT_TRANSPORT_TIMEOUT_S
 from .firetv.firetv_sync import FireTVSync
 
 
-__version__ = "0.0.70"
+__version__ = "0.0.71"
 
 
 def setup(
