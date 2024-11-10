8f40ded2e3591f55b3c3e0037f45a7d61db235e8
Jeff Irion <jefflirion@users.noreply.github.com>
2024-11-09 22:58:39 -0800

    Bump the version to 0.0.75

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst") as f:
 
 setup(
     name="androidtv",
-    version="0.0.74",
+    version="0.0.75",
     description="Communicate with an Android TV or Fire TV device via ADB over a network.",
     long_description=readme,
     keywords=["adb", "android", "androidtv", "firetv"],
